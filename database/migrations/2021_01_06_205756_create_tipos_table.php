98de6b5bde237be6121c3a43f1e9839cdad68d13
m4ury <mauriciomorales0410@gmail.com>
2021-01-07 07:47:20 -0300

    migraciones parte 1

---
+++ database/migrations/2021_01_06_205756_create_tipos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTiposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipos', function (Blueprint $table) {
+            $table->id();
+            $table->enum('tipo_nombre', ['Factura Venta', 'Factura Compra', 'Guia Despacho', 'Nota de Credito', 'Nota de Debito', 'Boleta']);
+            $table->string('tipo_descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipos');
+    }
+}
