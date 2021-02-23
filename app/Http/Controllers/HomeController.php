c871acf39519ba66e4de45c1e3d2010ff66efdbf
r3drum <mauriciomorales0410@gmail.com>
2021-02-23 16:54:39 -0300

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Paciente;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $totalPacientes = Paciente::all('rut')->count();
+        $totalMasculino = Paciente::where('sexo', '=', 'Masculino')->count();
+        $totalFemenino = Paciente::where('sexo', '=', 'Femenino')->count();
+
+        $totalCeleste = Paciente::where('sector', '=', 'celeste')->count();
+        $totalNaranjo = Paciente::where('sector', '=', 'naranjo')->count();
+
+
+        return view('home', compact('totalPacientes', 'totalMasculino', 'totalFemenino', 'totalCeleste', 'totalNaranjo'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,21 +6,92 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="row">
+                        <div class="col">
+                            <p>Total pacientes: {{ $totalPacientes  }}</p>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="col">
+                            <p>Total Masculino: {{ $totalMasculino  }}</p>
+                        </div>
+                        <div class="col">
+                            <p>Total Femenino: {{ $totalFemenino  }}</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <canvas id="myPieChart"></canvas>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+@section('js')
+    <script>
+        var ctx = document.getElementById('myPieChart').getContext('2d');
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Mujeres', 'Hombres'],
+                datasets: [{
+                    label: 'Q Pacientes por Sexo',
+                    data: [{{$totalFemenino}}, {{$totalMasculino}}],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)'],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)'
+                        ],
+                    borderWidth: 1
+                }]
+            }
+        });
+    </script>
+    {{--<script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>--}}
+    @endsection
