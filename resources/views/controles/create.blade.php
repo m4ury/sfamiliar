e0b6e68f69ed96a331e5b92de9315c7211df31fc
r3drum <mauriciomorales0410@gmail.com>
2021-03-09 17:11:08 -0300

    controles mejoras

--- resources/views/controles/create.blade.php
+++ resources/views/controles/create.blade.php
@@ -1,34 +1,35 @@
 @extends('adminlte::page')
 @section('title', 'nuevo-control')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-left">
-            <div class="col-sx-12 col-sm-12 col-lg-8">
-                <div class="card card-success card-outline">
-                    <div class="card-header">Nuevo Control</div>
-                    <div class="card-body">
-                        {{ Form::open(['action' => 'ControlController@store', 'method' => 'POST', 'class' => 'form-horizontal']) }}
-                        @include('controles.form')
-                        <div class="row">
-                            <div class="col">
-                                {{ Form::submit('Guardar', ['class' => 'btn bg-gradient-primary btn-sm btn-block', 'id' => '']) }}
-                            </div>
-                            <div class="col">
-                                <a href="{{ route('pacientes.show', $paciente->id) }}" style="text-decoration:none">
-                                    {{ Form::button('Cancelar', ['class' => 'btn bg-gradient-secondary btn-sm btn-block'] ) }}
-                                </a>
-                            </div>
+<div class="container">
+    <div class="row justify-content-left">
+        <div class="col-sx-12 col-sm-12 col-lg-8">
+            <div class="card card-success card-outline">
+                <div class="card-header"><label for="">Nuevo Control</label><input type="text" name="paciente"
+                        value="{{ $paciente->fullName() }}" class="form-control" disabled></div>
+                <div class="card-body">
+                    {{ Form::open(['action' => 'ControlController@store', 'method' => 'POST', 'class' => 'form-horizontal']) }}
+                    @include('controles.form')
+                    <div class="row">
+                        <div class="col">
+                            {{ Form::submit('Guardar', ['class' => 'btn bg-gradient-primary btn-sm btn-block', 'id' => '']) }}
                         </div>
-                        {{ Form::close() }}
-
-                        @section('js')
-                            <script>
-                                $('#comuna, #sexo, #sector').select2();
-                            </script>
-                        @endsection
+                        <div class="col">
+                            <a href="{{ route('pacientes.show', $paciente->id) }}" style="text-decoration:none">
+                                {{ Form::button('Cancelar', ['class' => 'btn bg-gradient-secondary btn-sm btn-block'] ) }}
+                            </a>
+                        </div>
                     </div>
+                    {{ Form::close() }}
+
+                    @section('js')
+                    <script>
+                        $('#comuna, #sexo, #sector').select2();
+                    </script>
+                    @endsection
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/controles/form.blade.php
+++ resources/views/controles/form.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group row">
-    {!! Form::label('tipo_control', 'Tipo Control', ['class' => 'col-sm-3 col-form-label']) !!}
+    {!! Form::label('tipo_control', 'Profesional', ['class' => 'col-sm-3 col-form-label']) !!}
     <div class="col-sm">
-        {!! Form::select('tipo_control', ['Medico'=> 'Medico', 'Enfermera' => 'Enfermera', 'Kinesiologo' => 'Kinesiologo', 'Nutricionista' => 'Nutricionista'], null, ['class' => 'form-control form-control-sm', 'id' => 'tipo', 'placeholder'=> "Seleccione Tipo Control"]) !!}
+        {!! Form::select('tipo_control', ['Medico'=> 'Medico', 'Enfermera' => 'Enfermera', 'Kinesiologo' => 'Kinesiologo', 'Nutricionista' => 'Nutricionista'], null, ['class' => 'form-control form-control-sm', 'id' => 'tipo', 'placeholder'=> "Seleccione Profesional"]) !!}
     </div>
 </div>
 
@@ -22,17 +22,21 @@
 <div class="form-group row">
     {!! Form::label('peso_actual', 'Peso actual(Kg.)',['class' => 'col-sm-3 col-form-label']) !!}
     <div class="col-sm-3">
-        {!! Form::number('peso_actual', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Peso en Kg.']) !!}
+        {!! Form::number('peso_actual', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ejemplo: 88']) !!}
     </div>
     {!! Form::label('talla_actual', 'Talla actual(Cms.)',['class' => 'col-sm-3 col-form-label']) !!}
     <div class="col-sm-3">
-        {!! Form::number('talla_actual', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Talla en Cms.']) !!}
+        {!! Form::number('talla_actual', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ejemplo: 175']) !!}
     </div>
 </div>
 <div class="form-group row">
     {!! Form::label('imc', 'IMC',['class' => 'col-sm-3 col-form-label']) !!}
     <div class="col-sm-3">
-        {!! Form::number('imc', null, ['class' => 'form-control form-control-sm','step' => 'any']) !!}
+        {!! Form::text('imc', null, ['class' => 'form-control form-control-sm','step' => 'any', 'placeholder' => 'IMC']) !!}
+    </div>
+    {!! Form::label('imc_resultado', 'Estado Nutricional',['class' => 'col-sm-3 col-form-label']) !!}
+    <div class="col-sm-3">
+        {!! Form::text('imc_resultado', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Est. Nutricional.']) !!}
     </div>
 </div>
 <div class="form-group row">
@@ -46,10 +50,6 @@
     {!! Form::label('proximo_control', 'Fecha prox. control',['class' => 'col-sm-3 col-form-label']) !!}
     <div class="col-sm-3">
         {!! Form::date('proximo_control', null, ['class' => 'form-control form-control-sm']) !!}
-    </div>
-    {!! Form::label('hora_proximoControl', 'Hora',['class' => 'col-sm-3 col-form-label']) !!}
-    <div class="col-sm-3">
-        {!! Form::time('hora_proximoControl', null, ['class' => 'form-control form-control-sm', 'placeholder']) !!}
     </div>
 </div>
 @section('js')
