ff4d741f86d0591df6f3d13c0f7d89461628fa0f
m4ury <mauriciomorales0410@gmail.com>
2022-02-23 17:09:15 -0300

    avance seccion c p4 estadisticas

--- resources/views/estadisticas/seccion-c.blade.php
+++ resources/views/estadisticas/seccion-c.blade.php
@@ -17,73 +17,169 @@
                 <table class="table table-md-responsive table-bordered">
                     <thead>
                         <tr>
-                            <th class="text-center" colspan="2" rowspan="2">VARIABLES</th>
-                            <th class="text-center" rowspan="2">TOTAL</th>
-                            <th class="text-center" colspan="2">EDAD (en años)</th>
-                            <th colspan="2">SEXO</th>
+                            <th class="text-center" colspan="2" rowspan="3">VARIABLES</th>
+                            <th class="text-center" colspan="3" rowspan="2">TOTAL</th>
+                            <th class="text-center" colspan="28">GRUPOS DE EDAD (en años) Y SEXO</th>
+                            <th colspan="2" rowspan="2">Pueblos Originarios</th>
+                            <th colspan="2" rowspan="2">Poblacion Migrantes</th>
                         </tr>
                         <tr>
-                            <th nowrap="">15 a 64 años</th>
-                            <th nowrap="">65 Y mas</th>
+                            <th nowrap="" colspan="2">15 a 19 años</th>
+                            <th nowrap="" colspan="2">20 a 24 años</th>
+                            <th nowrap="" colspan="2">25 a 29 años</th>
+                            <th nowrap="" colspan="2">30 a 34 años</th>
+                            <th nowrap="" colspan="2">35 a 39 años</th>
+                            <th nowrap="" colspan="2">40 a 44 años</th>
+                            <th nowrap="" colspan="2">45 a 49 años</th>
+                            <th nowrap="" colspan="2">50 a 54 años</th>
+                            <th nowrap="" colspan="2">55 a 59 años</th>
+                            <th nowrap="" colspan="2">60 a 64 años</th>
+                            <th nowrap="" colspan="2">65 a 69 años</th>
+                            <th nowrap="" colspan="2">70 a 74 años</th>
+                            <th nowrap="" colspan="2">75 a 79 años</th>
+                            <th nowrap="" colspan="2">80 y mas años</th>
+                        </tr>
+                        <tr>
+                            <th>Ambos Sexos</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
+                            <th>Hombres</th>
+                            <th>Mujeres</th>
                             <th>Hombres</th>
                             <th>Mujeres</th>
                         </tr>
                         <tr>
-                            <th class="text-left bg-gradient-gray" colspan="7">PERSONAS DIABETICAS EN PSCV</th>
+                            <th class="text-left bg-gradient-gray" colspan="2">PERSONAS DIABETICAS EN PSCV</th>
                         </tr>
                         <tr>
                             <th colspan="2">CON RAZON ALBÚMINA CREATININA (RAC),VIGENTE</th>
-                            <td>
-                                {{ $racVigente }}
-                            </td>
-                            <td>
-                                {{ $racVigente_1564 }}
-                            </td>
-                            <td>
-                                {{ $racVigente_65 }}
-                            </td>
-                            <td>
-                                {{ $racVigenteM }}
-                            </td>
-                            <td>
-                                {{ $racVigenteF }}
-                            </td>
+                            <td>{{ $racVigente }}</td>
+                            <td>{{ $racVigenteM }}</td>
+                            <td>{{ $racVigenteF }}</td>
+                            <td>{{$racVigente_1519M}}</td>
+                            <td>{{$racVigente_1519F}}</td>
+                            <td>{{$racVigente_2024M}}</td>
+                            <td>{{$racVigente_2024F}}</td>
+                            <td>{{$racVigente_2529M}}</td>
+                            <td>{{$racVigente_2529F}}</td>
+                            <td>{{$racVigente_3034M}}</td>
+                            <td>{{$racVigente_3034F}}</td>
+                            <td>{{$racVigente_3539M}}</td>
+                            <td>{{$racVigente_3539F}}</td>
+                            <td>{{$racVigente_4044M}}</td>
+                            <td>{{$racVigente_4044F}}</td>
+                            <td>{{$racVigente_4549M}}</td>
+                            <td>{{$racVigente_4549F}}</td>
+                            <td>{{$racVigente_5054M}}</td>
+                            <td>{{$racVigente_5054F}}</td>
+                            <td>{{$racVigente_5559M}}</td>
+                            <td>{{$racVigente_5559F}}</td>
+                            <td>{{$racVigente_6064M}}</td>
+                            <td>{{$racVigente_6064F}}</td>
+                            <td>{{$racVigente_6569M}}</td>
+                            <td>{{$racVigente_6569F}}</td>
+                            <td>{{$racVigente_7074M}}</td>
+                            <td>{{$racVigente_7074F}}</td>
+                            <td>{{$racVigente_7579M}}</td>
+                            <td>{{$racVigente_7579F}}</td>
+                            <td>{{$racVigente_80M}}</td>
+                            <td>{{$racVigente_80F}}</td>
                         </tr>
                         <tr>
                             <th colspan="2">CON VELOCIDAD DE FILTRACIÓN GLOMERULAR (VFG), VIGENTE</th>
-                            <td>
-                                {{ $vfgVigente }}
-                            </td>
-                            <td>
-                                {{ $vfgVigente_1564 }}
-                            </td>
-                            <td>
-                                {{ $vfgVigente_65 }}
-                            </td>
-                            <td>
-                                {{ $vfgVigenteM }}
-                            </td>
-                            <td>
-                                {{ $vfgVigenteF }}
-                            </td>
+                            <td>{{ $vfgVigente }}</td>
+                            <td>{{ $vfgVigenteM }}</td>
+                            <td>{{ $vfgVigenteF }}</td>
+                            <td>{{$vfgVigente_1519M}}</td>
+                            <td>{{$vfgVigente_1519F}}</td>
+                            <td>{{$vfgVigente_2024M}}</td>
+                            <td>{{$vfgVigente_2024F}}</td>
+                            <td>{{$vfgVigente_2529M}}</td>
+                            <td>{{$vfgVigente_2529F}}</td>
+                            <td>{{$vfgVigente_3034M}}</td>
+                            <td>{{$vfgVigente_3034F}}</td>
+                            <td>{{$vfgVigente_3539M}}</td>
+                            <td>{{$vfgVigente_3539F}}</td>
+                            <td>{{$vfgVigente_4044M}}</td>
+                            <td>{{$vfgVigente_4044F}}</td>
+                            <td>{{$vfgVigente_4549M}}</td>
+                            <td>{{$vfgVigente_4549F}}</td>
+                            <td>{{$vfgVigente_5054M}}</td>
+                            <td>{{$vfgVigente_5054F}}</td>
+                            <td>{{$vfgVigente_5559M}}</td>
+                            <td>{{$vfgVigente_5559F}}</td>
+                            <td>{{$vfgVigente_6064M}}</td>
+                            <td>{{$vfgVigente_6064F}}</td>
+                            <td>{{$vfgVigente_6569M}}</td>
+                            <td>{{$vfgVigente_6569F}}</td>
+                            <td>{{$vfgVigente_7074M}}</td>
+                            <td>{{$vfgVigente_7074F}}</td>
+                            <td>{{$vfgVigente_7579M}}</td>
+                            <td>{{$vfgVigente_7579F}}</td>
+                            <td>{{$vfgVigente_80M}}</td>
+                            <td>{{$vfgVigente_80F}}</td>
                         </tr>
                         <tr>
                             <th colspan="2">CON FONDO DE OJO, VIGENTE</th>
-                            <td>
-                                {{ $fondoOjoVigente }}
-                            </td>
-                            <td>
-                                {{ $fondoOjoVigente_1564 }}
-                            </td>
-                            <td>
-                                {{ $fondoOjoVigente_65 }}
-                            </td>
-                            <td>
-                                {{ $fondoOjoVigenteM }}
-                            </td>
-                            <td>
-                                {{ $fondoOjoVigenteF }}
-                            </td>
+                            <td>{{ $fondoOjoVigente }}</td>
+                            <td>{{ $fondoOjoVigenteM }}</td>
+                            <td>{{ $fondoOjoVigenteF }}</td>
+                            <td>{{$fondoOjoVigente_1519M}}</td>
+                            <td>{{$fondoOjoVigente_1519F}}</td>
+                            <td>{{$fondoOjoVigente_2024M}}</td>
+                            <td>{{$fondoOjoVigente_2024F}}</td>
+                            <td>{{$fondoOjoVigente_2529M}}</td>
+                            <td>{{$fondoOjoVigente_2529F}}</td>
+                            <td>{{$fondoOjoVigente_3034M}}</td>
+                            <td>{{$fondoOjoVigente_3034F}}</td>
+                            <td>{{$fondoOjoVigente_3539M}}</td>
+                            <td>{{$fondoOjoVigente_3539F}}</td>
+                            <td>{{$fondoOjoVigente_4044M}}</td>
+                            <td>{{$fondoOjoVigente_4044F}}</td>
+                            <td>{{$fondoOjoVigente_4549M}}</td>
+                            <td>{{$fondoOjoVigente_4549F}}</td>
+                            <td>{{$fondoOjoVigente_5054M}}</td>
+                            <td>{{$fondoOjoVigente_5054F}}</td>
+                            <td>{{$fondoOjoVigente_5559M}}</td>
+                            <td>{{$fondoOjoVigente_5559F}}</td>
+                            <td>{{$fondoOjoVigente_6064M}}</td>
+                            <td>{{$fondoOjoVigente_6064F}}</td>
+                            <td>{{$fondoOjoVigente_6569M}}</td>
+                            <td>{{$fondoOjoVigente_6569F}}</td>
+                            <td>{{$fondoOjoVigente_7074M}}</td>
+                            <td>{{$fondoOjoVigente_7074F}}</td>
+                            <td>{{$fondoOjoVigente_7579M}}</td>
+                            <td>{{$fondoOjoVigente_7579F}}</td>
+                            <td>{{$fondoOjoVigente_80M}}</td>
+                            <td>{{$fondoOjoVigente_80F}}</td>
                         </tr>
                         <tr>
                             <th colspan="2">CON ATENCIÓN PODOLÓGICA VIGENTE</th>
@@ -425,21 +521,37 @@
                         </tr>
                         <tr>
                             <th nowrap="" colspan="2">CON PRESIÓN ARTERIAL igual o Mayor 160/100 mmHg</th>
-                            <td>
-                                {{ $paMayor160 }}
-                            </td>
-                            <td>
-                                {{ $paMayor160_1564 }}
-                            </td>
-                            <td>
-                                {{ $paMayor160_65 }}
-                            </td>
-                            <td>
-                                {{ $paMayor160M }}
-                            </td>
-                            <td>
-                                {{ $paMayor160F }}
-                            </td>
+                            <td>{{ $paMayor160 }}</td>
+                            <td>{{ $paMayor160M }}</td>
+                            <td>{{ $paMayor160F }}</td>
+                            <td>{{$paMayor160_1519M}}</td>
+                            <td>{{$paMayor160_1519F}}</td>
+                            <td>{{$paMayor160_2024M}}</td>
+                            <td>{{$paMayor160_2024F}}</td>
+                            <td>{{$paMayor160_2529M}}</td>
+                            <td>{{$paMayor160_2529F}}</td>
+                            <td>{{$paMayor160_3034M}}</td>
+                            <td>{{$paMayor160_3034F}}</td>
+                            <td>{{$paMayor160_3539M}}</td>
+                            <td>{{$paMayor160_3539F}}</td>
+                            <td>{{$paMayor160_4044M}}</td>
+                            <td>{{$paMayor160_4044F}}</td>
+                            <td>{{$paMayor160_4549M}}</td>
+                            <td>{{$paMayor160_4549F}}</td>
+                            <td>{{$paMayor160_5054M}}</td>
+                            <td>{{$paMayor160_5054F}}</td>
+                            <td>{{$paMayor160_5559M}}</td>
+                            <td>{{$paMayor160_5559F}}</td>
+                            <td>{{$paMayor160_6064M}}</td>
+                            <td>{{$paMayor160_6064F}}</td>
+                            <td>{{$paMayor160_6569M}}</td>
+                            <td>{{$paMayor160_6569F}}</td>
+                            <td>{{$paMayor160_7074M}}</td>
+                            <td>{{$paMayor160_7074F}}</td>
+                            <td>{{$paMayor160_7579M}}</td>
+                            <td>{{$paMayor160_7579F}}</td>
+                            <td>{{$paMayor160_80M}}</td>
+                            <td>{{$paMayor160_80F}}</td>
                         </tr>
                         <tr>
                             <th colspan="7" class="text-left bg-gradient-gray">TODAS LAS PERSONAS EN PSCV</th>
