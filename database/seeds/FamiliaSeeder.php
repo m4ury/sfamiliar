c62e986f583e92bf23689658ede93485ff3da628
m4ury <mauriciomorales0410@gmail.com>
2022-08-26 14:10:15 -0400

    prueba de seeder familia ok

--- database/seeds/FamiliaSeeder.php
+++ database/seeds/FamiliaSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Familia;
 use Illuminate\Database\Seeder;
 
 class FamiliaSeeder extends Seeder
@@ -11,6 +12,12 @@ class FamiliaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $familias = [
+            ['familia' => 'MORALES CUBILLOS', 'domicilio' => 'Leonera S/N', 'ficha_familiar' => '11111', 'fono' => 999999999, 'num_integrantes' => 3, 'sector' => 'SA'],
+        ];
+
+        foreach ($familias as $familia) {
+            Familia::updateOrCreate($familia);
+        }
     }
 }
